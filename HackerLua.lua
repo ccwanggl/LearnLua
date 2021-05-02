92babb287930981d68d544ec4bc6fdf1286e1e45
guoliang wang <tswanggl@hotmail.com>
2021-05-03 01:05:45 +0800

    special cases in table keys

--- HackerLua.lua
+++ HackerLua.lua
@@ -140,3 +140,18 @@ t = {key1 = 'value1', key2 = false}
 print(t.key1)           -- Prints 'value1'.
 t.newKey = {}           -- Adds a new key/value pair.
 t.key2 = nil            -- Remove key2 from the table.
+
+
+-- Literal notation for any (non-nil) value as key:
+u = {['@!#'] = 'qbert', [{}] = 1729, [6.28 = 'tau'}
+print(u[6.28])      -- prints "tau"
+
+-- Key matching is basically by value for numbers
+-- and strings, but by identify for tables.
+
+a = u['@!#']        -- Now a = 'qbert'.
+b = u[{}]           -- We might expect 1729, but it's nil:
+-- b = nil since the lookup fails. It fails
+-- because the key we used is not the same object
+-- as the one used to store the original value. So
+-- strings & numbers are more portable keys.
