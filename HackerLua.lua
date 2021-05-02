30a3c2673002e5628f3942fb8715e2c62b48f09d
guoliang wang <tswanggl@hotmail.com>
2021-05-03 00:26:33 +0800

    general range write format 	begin,end[, step]

--- HackerLua.lua
+++ HackerLua.lua
@@ -58,3 +58,8 @@ karlSum = 0
 for i = 1, 100 do -- The range includes both ends.
     kerlSum = kerlSum + i
 end
+
+-- In general, the range is "begin, end[, step]".
+-- Use "100, 1, -1" as the range to count down:
+fredSum = 0
+for j = 100, 1, -1 do fredSum = fredSum + j end
