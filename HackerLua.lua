ce56a472b844399e8834b9675b0c4a9214099cf7
guoliang wang <tswanggl@hotmail.com>
2021-05-03 00:36:48 +0800

    anonymous functions

--- HackerLua.lua
+++ HackerLua.lua
@@ -77,3 +77,20 @@ function fib(n)
     if n < 2 then return 1 end
     return fib(n - 2) + fib(n - 1)
 end
+
+-- Closures and anonyumous functions are ok:
+function adder(x)
+    -- The returned function is created when adder is
+    -- called, and remembers the value of x:
+    return function(y) return  x + y end
+end
+
+a1 = adder(9)
+a2 = adder(36)
+print(a1(16))       --> 25
+print(a2(64))       --> 100
+
+-- Returns, func calls, and assignments all work
+-- with lists that may be mismatched in length,.
+-- Unmatched receivers are nil;
+-- unmatched senders are discarded.
