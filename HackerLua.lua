5b04e85ad7a672d5ca2530cf75e3cd2ec1b2e52d
guoliang wang <tswanggl@hotmail.com>
2021-05-03 00:51:39 +0800

    call function 	1., one string parameter 	2. ...

--- HackerLua.lua
+++ HackerLua.lua
@@ -111,4 +111,13 @@ x, y = bar('zaphod')    --> prints "zaphod nil nil"
 function f(x) return x * x end
 f = function (x) return x * x end
 
+-- And so are these:
+local function g(x) return math.sin(x) end
+local g; g = function (x) return math.sin(x) end
 
+-- the 'local g' decl makes g-self-references ok.
+-- Trig funcs work in radians, by the way.
+-- Calls with one string param don't need parens:
+
+print 'hello' -- Works find.
+    
