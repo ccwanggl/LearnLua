d39926bf0d7cbce26868b3b32d10b37bb1c34f2f
guoliang wang <guoliang>
2021-05-03 19:21:37 +0800

    Access table item 	for key, val in pairs(u) do 	... 	end

--- HackerLua.lua
+++ HackerLua.lua
@@ -155,3 +155,12 @@ b = u[{}]           -- We might expect 1729, but it's nil:
 -- because the key we used is not the same object
 -- as the one used to store the original value. So
 -- strings & numbers are more portable keys.
+
+-- A one-table-param function call needs no parens:
+function h(x) print(x.key1) end
+h{key1 = 'Sonmi~451'}       -- Prints 'Sonmi~451'.
+
+for key, val in pairs(u) do 
+    print(key, val)
+end
+
