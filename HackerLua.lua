eb6aed01b7f0d8a2b2b464f7638959c15566d56e
guoliang <tswanggl@gmail.com>
2021-12-16 12:44:52 +0800

    function

--- HackerLua.lua
+++ HackerLua.lua
@@ -169,30 +169,41 @@ local a2 = adder(36)
 print(a1(16))       --> 25
 print(a2(64))       --> 100
 
--- Returns, func calls, and assignments all work
--- with lists that may be mismatched in length,.
+-- Returns, func calls, and assignments all work with lists that may be mismatched in length,.
 -- Unmatched receivers are nil;
 -- unmatched senders are discarded.
 
-x, y, z = 1, 2, 3, 4
+local x, y, z = 1, 2, 3, 4
 -- Now x = 1, y = 2, z = 3, and 4 is thrown away.
 
-function bar(a, b, c)
+local function bar(a, b, c)
     print(a,b,c)
     return 4, 8, 15, 16, 23, 42
 end
 
-x, y = bar('zaphod')    --> prints "zaphod nil nil"
+local x, y = bar('zaphod')    --> prints "zaphod nil nil"
 -- Now x = 4, y = 8, values 15...42 are discarded.
 
 -- Functions are first-class, may be local/global.
 -- These are the same:
-function f(x) return x * x end
-f = function (x) return x * x end
+local function f(x)
+    return x * x
+end
+
+local f = function (x)
+    return x * x
+end
 
 -- And so are these:
-local function g(x) return math.sin(x) end
-local g; g = function (x) return math.sin(x) end
+local function g(x)
+    return math.sin(x)
+end
+
+
+local g
+g = function (x)
+    return math.sin(x)
+end
 
 -- the 'local g' decl makes g-self-references ok.
 -- Trig funcs work in radians, by the way.
