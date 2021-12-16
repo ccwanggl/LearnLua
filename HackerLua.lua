cdc52ebef89fbd4e9d924c08a208fdf44068cda8
guoliang <tswanggl@gmail.com>
2021-12-16 12:14:46 +0800

    some basic operation

--- HackerLua.lua
+++ HackerLua.lua
@@ -42,16 +42,16 @@ end
 -- If clauses:
 if num > 40 then
     print('over 40')
-elseif s ~= 'walternate' then -- ~= is not equals.
-    -- Equality check is == like Python: ok for strs,.
-    io.write('not over 40\n') -- Defaults to stdout.
+elseif s ~= 'walternate' then       -- ~= is not equals.
+                                    -- Equality check is == like Python: ok for strs,.
+    io.write('not over 40\n')       -- Defaults to stdout.
 else
-    -- Variables are global by default.
-    ThisIsGlobal = 5 -- Camel case is common.
-    -- How to make a variable local:
-    local line = io.read() -- Read next stdin line.
+                                    -- Variables are global by default.
+    ThisIsGlobal = 5                -- Camel case is common.
+                                    -- How to make a variable local:
+    local line = io.read()          -- Read next stdin line.
 
-    -- String concatenation uses the .. operator:
+                                    -- String concatenation uses the ".." operator:
     print('Winter is coming,' .. line)
 end
 
